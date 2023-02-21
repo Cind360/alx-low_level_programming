06ebab697a2bb0f813d2a6a6633d24547319f08a
cind360 <cindymutunga@gmail.com>
2023-02-21 09:02:55 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * _putchar - write the character c to stdout
+ * gc: the character to print 
+ * Return: on success 1.
+ * on error, -1 is returned and the error set appropriately
+ */
+int_putchar(char c)
+{
+ 
+	return (write(1,$c, 1));
+
+}	
